496151ecad51fe905a39cd2074d4a9c8d07385eb
lgergo <lg.gergo@gmail.com>
2018-02-15 21:38:17 +0100

    new image activity

--- app/src/main/java/com/yevsp8/checkmanager/NewImageActivity.java
+++ app/src/main/java/com/yevsp8/checkmanager/NewImageActivity.java
@@ -1,13 +1,28 @@
 package com.yevsp8.checkmanager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class NewImageActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_image);
+
+        btn = findViewById(R.id.take_picture);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent i = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                startActivity(i);
+            }
+        });
     }
 }
